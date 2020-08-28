1e103128673b8046ba4dbdb24c072e96c2746471
edwinmsihotang <60915814+edwinmsihotang@users.noreply.github.com>
2020-08-28 08:05:06 +0700

    Update ulliulli.js

--- ulliulli.js
+++ ulliulli.js
@@ -3751,8 +3751,8 @@ $('.about-author .author-description span a').each(function() {
     var $this = $(this),
         cls = $this.text().trim(),
         url = $this.attr('href');
-    $this.replaceWith('<li class="' + cls + '"><a href="' + url + '" title="' + cls + '" rel="noopener" target="_blank"/></li>').wrap('<ul></ul>');
-    $('.author-description').append($('.author-description span li'));
+    $this.replaceWith('<li class="' + cls + '"><a href="' + url + '" title="' + cls + '" rel="noopener" target="_blank"/></li>');
+    $('.author-description').append('<li></li>').append($('.author-description span li'));
     $('.author-description').addClass('show-icons')
 });
 
